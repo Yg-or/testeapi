7d35088bcef653b62808dde216a45f5d2faa6a71
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-09-30 18:50:47 -0300

    teste

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(routes)
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 22162, () => {
   console.log('Express Started')
 });
 
